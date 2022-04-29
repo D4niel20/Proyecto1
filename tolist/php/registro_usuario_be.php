9a91125f292ae043bc53be8199190045ade021de
D4niel20 <201804338i@utea.edu.pe>
2022-04-29 18:34:51 -0500

    noveno commit

--- tolist/php/registro_usuario_be.php
+++ tolist/php/registro_usuario_be.php
@@ -8,6 +8,30 @@ include 'conexion_be.php';
 
     $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena)
             VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
+    //verificar que el correo no se duplique en la base de datos
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' ");
+    
+    if(mysqli_num_rows($verificar_correo) >0){
+    echo'
+        <script>
+            alert("correo ya registrado, Ingrese otro correo");
+            window.loction = "../index.php";
+        </script>
+    ';
+    exit();
+}
+    //verificar que el nombre del usuario no se duplique en la base de datos
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo' ");
+    
+    if(mysqli_num_rows($verificar_usuario) >0){
+    echo'
+        <script>
+            alert("este usuario ya registrado, Ingrese otro usuario");
+            window.loction = "../index.php";
+        </script>
+    ';
+    exit();
+}
 
     $ejecutar = mysqli_query($conexion, $query);
 
